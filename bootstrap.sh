9bcd72c4005e3195933c9407002ebd8f3644364b
Tomasz Kuryłek <tkurylek@future-processing.com>
2015-03-03 12:05:49 +0100

    Fixed responsive design

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,6 +2,8 @@
 yum -y install ruby
 yum -y install gcc g++ make automake autoconf curl-devel openssl-devel zlib-devel httpd-devel apr-devel apr-util-devel sqlite-devel
 yum -y install ruby-rdoc ruby-devel
+## Centos 6
+#yum -y install rubygems
 gem install jekyll
 yum -y install epel-release
 yum -y install npm
